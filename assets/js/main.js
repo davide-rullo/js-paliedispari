e6ee291cff3035d0c1b1dcdfb462d5922ae61381
davide-rullo <davide.rullo01@gmail.com>
2023-08-31 19:54:29 +0200

    fix
    fixed oddoreven script

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari 
 (usando una funzione) Dichiariamo chi ha vinto.
 */
-/*
+
 const userChoice = prompt("pari o dispari?");
 const userNumber = Number(prompt("Scegli un numero da 1 a 5"));
 
@@ -23,36 +23,39 @@ console.log("Il totale è " + sum);
 
 function evenOrOdd() {
     if (sum % 2 === 0) {
-        result = "pari"
+        return "pari";
     } else {
-        result = "dispari"
+        return "dispari";
     }
-    return result;
+    
 }
 
-let finalResult = evenOrOdd();
+let result = evenOrOdd();
 
-if (userChoice %2 === 0 && result === "pari") {
-    console.log('Pari, hai vinto');
-} else if (userChoice % 2 === 0 && result === "dispari") {
-    console.log("Pari, vince la CPU");
-} else if (userChoice % 2 !== 0 && result === "pari") {
-    console.log("Dispari, vince la CPU");
-} else if (userChoice % 2 !== 0 && result === "dispari") {
+if (userChoice === "pari" && result === "pari") {
+    console.log('Pari, hai vinto!');
+} else if (userChoice === "dispari" && result === "dispari") {
     console.log("Dispari, hai vinto!");
+} else if (userChoice === "pari" && result === "dispari") {
+    console.log("Dispari, vince la CPU");
+} else if (userChoice === "dispari" && result === "pari") {
+    console.log("Pari, vince la CPU");
 } 
-*/
+
 
 
  /*Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire
  se la parola inserita è palindroma*/
 
+/*
+
 const userWord = prompt("scrivi una parola, controllerò se è palindroma");
 let wordSum ="";
 
 function isItPalindrome() {
     for (let i = userWord.length - 1; i >= 0; i--) {
+        console.log(userWord[i]);
         wordSum += userWord[i]
         console.log(wordSum);
     }
@@ -65,3 +68,4 @@ function isItPalindrome() {
 }
 
 isItPalindrome();
+*/
